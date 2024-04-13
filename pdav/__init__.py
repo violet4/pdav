a801a3ef0bc0a77feedae442f1bddc4ec8aa6eff
Violet Eldridge <github@violet-mica.com>
2024-04-12 18:24:45 -0700

    add text_to_df (improved version of lines_table_to_dataframe)

--- pdav/__init__.py
+++ pdav/__init__.py
@@ -40,8 +40,8 @@ def lines_table_to_dataframe(lines, ignore_indices=[]) -> Tuple[pd.DataFrame,lis
 
     consecutive = calculate_first_consecutive_indices(empty_indices)
     if ignore_indices:
-        for ii in ignore_indices:
-            consecutive.remove(ii)
+        for ii in sorted(ignore_indices, reverse=True):
+            del consecutive[ii]
     for i in consecutive:
         array[:,i]='|'
 
@@ -52,3 +52,64 @@ def lines_table_to_dataframe(lines, ignore_indices=[]) -> Tuple[pd.DataFrame,lis
     df = pd.read_csv(sio, sep='|')
     df.columns = [c.strip() for c in df.columns]
     return df, consecutive
+
+
+def text_to_df(text: str) -> pd.DataFrame:
+    """
+    improved version of lines_table_to_dataframe
+    """
+    lines = text.split('\n')
+    max_len = max(map(len, lines))
+
+    lines = [l.ljust(max_len) for l in lines]
+    lines = [[c for c in line] for line in lines]
+    array = np.array(lines)
+
+    joined = None  # type: ignore
+    for row in array==' ':
+        if joined is None:
+            joined = row
+            continue
+        joined &= row
+    indices = []
+    joined: np.ndarray
+    for i, v in enumerate(joined):
+        if v:
+            indices.append(i)
+    # print(indices)
+
+    separate_indices = indices.copy()
+    i = 0
+    while i < len(separate_indices):
+        # either shorten indices..
+        if separate_indices[i-1]+1==separate_indices[i]:
+            del separate_indices[i-1]
+        # or increment i
+        else:
+            i += 1
+
+    new_lines = []
+    newer_lines = text.split('\n')
+    rows = []
+    full_indices = [0]+separate_indices+[None]
+    for line in newer_lines:
+        row = []
+        for i,j in zip(full_indices, full_indices[1:]):
+            row.append(line[i:j].strip())
+        rows.append(row)
+
+    df = pd.DataFrame(rows[1:], columns=rows[0])
+
+    bools = (df=='').all().to_frame().reset_index().rename(columns={'index':'header',0:'bool'})
+    merge_cols = bools.loc[lambda sdf: sdf['bool']].index.tolist()
+    keep_cols = bools.loc[lambda sdf: ~sdf['bool']].index.tolist()
+
+    new_df = df.copy()
+    columns = new_df.columns.tolist()
+    for i in merge_cols:
+        columns[i+1] = columns[i]+' '+columns[i+1]
+    new_df.columns = columns
+
+    final_df = new_df.iloc[:,keep_cols]
+    final_df = final_df.apply(pd.to_numeric, errors='ignore')
+    return final_df
